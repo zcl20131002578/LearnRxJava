087e870cf66696f2bad1e99b2a983c215c3fbc70
chunliangzhang <INKE20131002578>
2018-12-11 17:08:05 +0800

    text visable relation with child and parent

--- app/src/main/java/com/inke/zcl/learnrxjava/utils/GlobalContext.java
+++ app/src/main/java/com/inke/zcl/learnrxjava/utils/GlobalContext.java
@@ -1,4 +1,14 @@
 package com.inke.zcl.learnrxjava.utils;
 
 public class GlobalContext {
+
+    private static final GlobalContext outInstance = new GlobalContext();
+
+    private GlobalContext() {
+    }
+
+    public static GlobalContext getInstance() {
+        return outInstance;
+    }
+
 }
