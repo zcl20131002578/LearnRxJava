c296b80ecb6466241247afeec9272757da60119f
chunliangzhang <INKE20131002578>
2019-07-04 10:52:53 +0800

    [内存泄漏] text use of leakCanary

--- app/src/main/java/com/inke/zcl/learnrxjava/activity/LeakActivity.java
+++ app/src/main/java/com/inke/zcl/learnrxjava/activity/LeakActivity.java
@@ -1,11 +1,20 @@
 package com.inke.zcl.learnrxjava.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import com.inke.zcl.learnrxjava.R;
 import com.inke.zcl.learnrxjava.manager.LeakActivityMgr;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import rx.subscriptions.CompositeSubscription;
 
 /**
  * Create By chunliangzhang on 2019-06-25
@@ -14,15 +23,44 @@ import com.inke.zcl.learnrxjava.manager.LeakActivityMgr;
  */
 public class LeakActivity extends Activity {
 
+    public static final String TAG = "LeakActivity";
+    private ExecutorService executorService;
+    private final CompositeSubscription compositeSubscription = new CompositeSubscription();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leak_layout);
+        init();
         LeakActivityMgr.getInstance().addActivity(this);
+    }
+
+    private void init() {
+        try {
+            throwError(null);
+            this.executorService = Executors.newSingleThreadExecutor();
+            if (executorService == null) {
+                Log.d(TAG, "init: executorService=null");
+            } else {
+                Log.d(TAG, "init: executorService!=null");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void throwError(Context context) {
+        try {
+            SharedPreferences var3 = context.getSharedPreferences("share_data", 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        compositeSubscription.unsubscribe();
+        compositeSubscription.clear();
     }
 }
