a85f9f930c05e53a6a1aa7180c7b97cf3b765461
chunliangzhang <INKE20131002578>
2019-10-14 17:46:00 +0800

    [Android Plugin]MVP Helper

---
+++ app/src/main/java/com/inke/zcl/learnrxjava/mvp/contract/MVPContract.java
@@ -0,0 +1,19 @@
+package com.inke.zcl.learnrxjava.mvp.contract;
+
+/**
+ * Create By chunliangzhang on 2019-10-14
+ * Version 1.0
+ * Description: 生成MVP的模板
+ * notice：这个类以及上边的文件夹需要以Contract结尾。
+ */
+public interface MVPContract {
+
+    interface Model {
+    }
+
+    interface View {
+    }
+
+    interface Presenter {
+    }
+}
---
+++ app/src/main/java/com/inke/zcl/learnrxjava/mvp/model/MVPModel.java
@@ -0,0 +1,11 @@
+package com.inke.zcl.learnrxjava.mvp.model;
+
+import com.inke.zcl.learnrxjava.mvp.contract.MVPContract;
+
+/**
+ * Create By chunliangzhang on 2019-10-14
+ * Version 1.0
+ * Description:
+ */
+public class MVPModel implements MVPContract.Model {
+}
