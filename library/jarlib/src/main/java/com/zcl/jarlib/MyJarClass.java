9b321576e12bc2e6f5b68b41f64e2d382eb7a8aa
chunliangzhang <INKE20131002578>
2019-11-07 17:49:33 +0800

    更新gradle 配置

---
+++ library/jarlib/src/main/java/com/zcl/jarlib/MyJarClass.java
@@ -0,0 +1,9 @@
+package com.zcl.jarlib;
+
+public class MyJarClass {
+    public static final String TAG = "MyJarClass";
+
+    public static String doSomethingJava(String str) {
+        return str;
+    }
+}
