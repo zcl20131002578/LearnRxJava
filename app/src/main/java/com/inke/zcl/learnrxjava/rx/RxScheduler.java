8df67c3f66dd251973660a35da0d692644ba8031
chunliangzhang <INKE20131002578>
2019-11-21 15:12:09 +0800

    RxScheduler

---
+++ app/src/main/java/com/inke/zcl/learnrxjava/rx/RxScheduler.java
@@ -0,0 +1,17 @@
+package com.inke.zcl.learnrxjava.rx;
+
+
+import rx.Scheduler;
+import rx.schedulers.Schedulers;
+
+/**
+ * Create By chunliangzhang on 2019-11-19
+ * Version 1.0
+ * Description:
+ */
+public class RxScheduler {
+
+    public static void mainScheduler(){
+
+    }
+}
